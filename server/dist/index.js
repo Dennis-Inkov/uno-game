5772750430f9f441cf89eb4aaac711c2fb771463
DennisInkov <135211473+DennisInkov@users.noreply.github.com>
2026-04-20 11:38:03 +0500

    Remove deployment files, untrack dist directories

--- server/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
-const server = (0, server_1.createApp)();
-server.listen(PORT, () => {
-    console.log(`[UNO] Server running on port ${PORT}`);
-});
